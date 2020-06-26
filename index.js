a8885389c71351a681b752f09118a27cefbfa61a
AnaJL <analinsdev@gmail.com>
2020-06-26 19:30:14 -0300

    melhorando js

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 var coll = document.getElementsByClassName("collapsible");
+var main = document.querySelector(".main");
+var toshow =''
 var i;
 
+function toShow(){
+for (i=0; i<3;i++){
+    toshow +=
+    `<button class="collapsible">Open Section ${i+1}</button>
+    <div class="content">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>`
+}
+main.innerHTML = toshow
+}
+
+function Collapse(){
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
@@ -14,4 +28,8 @@ for (i = 0; i < coll.length; i++) {
       this.style.borderRadius = "30px 30px 0px 0px"
     } 
   });
-}
+}}
+toShow()
+Collapse()
+
+
