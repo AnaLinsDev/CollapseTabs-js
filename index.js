9d30bb70a84c584c147b287715d79ceff4769cd3
AnaJL <analinsdev@gmail.com>
2020-06-27 13:41:53 -0300

    simplificando function Collapse no JS

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var i;
 function toShow(){
 for (i=0; i<3;i++){
     toshow +=
-    `<button class="collapsible">Open Section ${i+1}</button>
+    `<button class="collapsible" onclick=(Collapse(${i}))>Open Section ${i+1}</button>
     <div class="content">
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
     </div>`
@@ -14,22 +14,19 @@ for (i=0; i<3;i++){
 main.innerHTML = toshow
 }
 
-function Collapse(){
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
+function Collapse(i){
+    var content = coll[i].nextElementSibling;
     if (content.style.maxHeight){
       content.style.maxHeight = null;
-      this.style.borderRadius = "0px"
+      coll[i].style.borderRadius = "0px"
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
       content.style.borderRadius = "0px 0px 30px 30px"
-      this.style.borderRadius = "30px 30px 0px 0px"
+      coll[i].style.borderRadius = "30px 30px 0px 0px"
     } 
-  });
-}}
+  };
+
 toShow()
-Collapse()
+
 
 
